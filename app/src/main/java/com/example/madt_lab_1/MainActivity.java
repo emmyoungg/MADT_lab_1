8893fe9e6d66c834f74853d97bd898876e0be08e
emmyoungg <56218355+emmyoungg@users.noreply.github.com>
2020-10-03 12:50:00 +0300

    Updated button to change text

--- app/src/main/java/com/example/madt_lab_1/MainActivity.java
+++ app/src/main/java/com/example/madt_lab_1/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.madt_lab_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.textView);
+    }
+
+    public void onButtonChangeClick(View view) {
+        this.tvMain.setText("We did something! Yay!");
     }
 }
